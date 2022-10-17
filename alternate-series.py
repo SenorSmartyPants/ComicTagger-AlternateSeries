fa671796f7ee017c8966340b1dbea2a8031c0bd0
SenorSmartyPants <senorsmartypants@gmail.com>
2022-10-17 13:41:43 -0500

    Change to LSIO Docker paths - add CT utils file
    Hotio mylar container has been abandoned by Hotio.
    Added comictagger_utils.py from ComicTagger-MarkScannerPage.

--- alternate-series.py
+++ alternate-series.py
@@ -1,42 +1,12 @@
 #!/usr/bin/python3
 import sys
-#using hotio docker paths
-
-#something is weird with the path. Not finding the libs but this path is at the end of the path list
-#insert into the start of path and then script works
-sys.path.insert(0,'/app/lib')
-
-from comictaggerlib.settings import *
-from comictaggerlib.comicarchive import *
-
+from comictagger_utils import *
 from titleparsing import *
 
 def main():
     print(__name__)
-    settings = ComicTaggerSettings('/config/app/.ComicTagger')
-
-    if len(sys.argv) < 2:
-        print("Usage: {0} [comicfile]".format(sys.argv[0]), file=sys.stderr)
-        return
-
-    filename = sys.argv[1]
-
-    if not os.path.exists(filename):
-        print(filename + ": not found!", file=sys.stderr)
-        return
-
-    #image path needed to start ComicArchive, not sure why.
-    #default image path is null in settings
-    ca = ComicArchive(
-        filename,
-        settings.rar_exe_path,
-        ComicTaggerSettings.getGraphic('nocover.png'))
-
-    if not ca.seemsToBeAComicArchive():
-        print("Sorry, but " + \
-            filename + " is not a comic archive!", file=sys.stderr)
-        return
-
+    filename = getFilename()
+    ca = getComicArchive(filename)
     style = MetaDataStyle.CIX #ComicRack Style metadata
 
     if ca.hasMetadata( style ):
@@ -46,7 +16,7 @@ def main():
         print("Title={0}".format(md.title))
         print("SA={0}".format(md.storyArc))
         print("AS={0} #{1}".format(md.alternateSeries, md.alternateNumber))
-        
+
         #move story arc to Alternate series
         #then process for numbers
         if md.alternateSeries is None:
@@ -64,15 +34,9 @@ def main():
             print("SA={0}".format(md.storyArc))
             print("AS={0} #{1}".format(md.alternateSeries, md.alternateNumber))
 
-            if not ca.writeMetadata(md, style):
-                print("The tag save seemed to fail!", file=sys.stderr)
-                return False
-            else:
-                print("Save complete.", file=sys.stderr)
-    
+            writeMetadata(md, ca, style)
     else:
         print("Comic archive does not have metadata", file=sys.stderr)
-
 
 if __name__ == '__main__':
     main()
--- mylar-wrapper.sh
+++ mylar-wrapper.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-/usr/bin/python3 /app/comictagger.py -S /storage/Comics/ComicTagger-AlternateSeries/alternate-series.py "$4"
+# $1 nzbname = original nzb name
+# $2 nzbpath = path to the nzb before Mylar moves it
+# $3 newfilename = the name given to the file by Mylar
+# $4 newpath = path to the file after Mylar has processed it
+# $5 metadata = seriesmetadata { name, comicyear, comicid , issueid, issueyear, issue, publisher } (didn't use it but appears to be the same as you state from a casual glance).
+
+/usr/bin/python3 /app/mylar3/comictagger.py -S /scripts/ComicTagger-AlternateSeries/alternate-series.py "$4"
